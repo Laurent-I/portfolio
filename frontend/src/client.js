c41c8c717591262e7c5cd4ae541306ed57c8c06a
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-11-08 20:36:50 +0200

    Connected frontend with sanity

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,9 +2,9 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "",
-  dataset: "",
-  apiVersion: "",
+  projectId: "zn1p6b8j",
+  dataset: "production",
+  apiVersion: "2022-02-01",
   useCdn: true,
-  token: "",
+  token: "skuhWTUOjiZLXssfXptU4JZewc8EpZ706FN6sg2dlWgMO8kdV7x7vdqp7qvsMurp2u5b7dHJ9kQsqUdpaIe2Lf1P6EhWsZDZpR7Spa6uSqloYP36GzwaekXRMmIb52fBoAUhXIMnsdndXxUAZsen1Y3jnPupu8yh64l4KDgeGcvAQc36iS3w",
 });
