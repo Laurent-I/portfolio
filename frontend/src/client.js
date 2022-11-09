c094eb57e531845b7693353bf0c824b328f7fa61
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-11-09 22:06:10 +0200

    connected to database

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "zn1p6b8j",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: "skuhWTUOjiZLXssfXptU4JZewc8EpZ706FN6sg2dlWgMO8kdV7x7vdqp7qvsMurp2u5b7dHJ9kQsqUdpaIe2Lf1P6EhWsZDZpR7Spa6uSqloYP36GzwaekXRMmIb52fBoAUhXIMnsdndXxUAZsen1Y3jnPupu8yh64l4KDgeGcvAQc36iS3w",
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,33 +2,41 @@ import { motion } from "framer-motion";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Frontend development",
-    description: "I am a competent frontend developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Python Developer",
-    description: "Scripting with python is kinda my thing",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Backend Development",
-    description: "Using Laravel in its full glory",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Analytical Thinking",
-    description: "I love the problems and all that come with them",
-    imgUrl: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Frontend development",
+//     description: "I am a competent frontend developer",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Python Developer",
+//     description: "Scripting with python is kinda my thing",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "Backend Development",
+//     description: "Using Laravel in its full glory",
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: "Analytical Thinking",
+//     description: "I love the problems and all that come with them",
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query, {}).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -44,7 +52,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
